2a1baf47ea5af3be00c0769ab3370944579ffc64
Moritz Johner <beller.moritz@googlemail.com>
2018-01-10 22:23:05 +0100

    feature: beta-ci

--- ci/test-image/build.sh
+++ ci/test-image/build.sh
@@ -2,4 +2,5 @@
 
 git -C kitt pull || git clone git@github.com:figome/kitt.git
 
-docker build -t figo/smcl-test .
+docker build -t eu.gcr.io/figo-va/smcl-test .
+docker push eu.gcr.io/figo-va/smcl-test
